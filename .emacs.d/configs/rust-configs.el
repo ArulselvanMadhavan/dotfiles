bfe86c7130983c3787c05523d2e0c3e39f74d547
Arul Madhavan <arul.madhavan@arulmadhavan-MBP.local>
2018-06-07 18:56:33 -0700

    Updated rust config

--- .emacs.d/configs/rust-configs.el
+++ .emacs.d/configs/rust-configs.el
@@ -1,23 +1,28 @@
 ;; Rust Setup
-(use-package cargo
-  :init
-  (add-hook 'rust-mode-hook 'cargo-minor-mode)
-  (add-hook 'toml-mode-hook 'cargo-minor-mode))
+;; (use-package cargo
+;;   :init
+;;   (add-hook 'rust-mode-hook 'cargo-minor-mode)
+;;   (add-hook 'toml-mode-hook 'cargo-minor-mode))
 
-(use-package racer
-  :init
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
+;; (use-package racer
+;;   :init
+;;   (add-hook 'rust-mode-hook #'racer-mode)
+;;   (add-hook 'racer-mode-hook #'eldoc-mode)
+;;   (add-hook 'racer-mode-hook #'company-mode)
+;;   :config
+;;   (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;;   (setq racer-cmd "~/.cargo/bin/racer"))
+
+;; ;; FlyCheck for rust
+;; (use-package flycheck-rust
+;;   :ensure t
+;;   :init
+;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package rust-mode
   :config
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  (setq racer-cmd "~/.cargo/bin/racer"))
-
-;; FlyCheck for rust
-(use-package flycheck-rust
-  :ensure t
-  :init
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+  (setq rust-format-on-save t))
 
 ;; (use-package rust-mode)
 ;; (add-hook 'rust-mode-hook (lambda()
