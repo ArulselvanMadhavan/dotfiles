cdd5bebd84c754dfa0fb53ff913b4ce51f807130
Arulselvan Madhavan <arulselvan1234@gmail.com>
2024-02-24 18:28:18 -0800

    check in

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,14 @@ status --is-interactive; and pyenv virtualenv-init - | source
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/arul/mambaforge/bin/conda
-    eval /home/arul/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+if test -f /home/arul/dev/cs152/lab1/conda/bin/conda
+    eval /home/arul/dev/cs152/lab1/conda/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/arul/dev/cs152/lab1/conda/etc/fish/conf.d/conda.fish"
+        . "/home/arul/dev/cs152/lab1/conda/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/arul/dev/cs152/lab1/conda/bin" $PATH
+    end
 end
 # <<< conda initialize <<<
 
--- .zshrc
+++ .zshrc
@@ -22,3 +22,19 @@ export LOCALBIN="$HOME/.local/bin"
 export PATH="$LOCALBIN:$CASK:$PATH"
 # Just start fish shell
 # exec fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/arul/dev/cs152/lab1/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/arul/dev/cs152/lab1/conda/etc/profile.d/conda.sh" ]; then
+        . "/home/arul/dev/cs152/lab1/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/arul/dev/cs152/lab1/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
